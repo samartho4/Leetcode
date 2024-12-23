dfef91d066e29524c8a2da3abe79f39e5b49aeef
samartho4 <142962370+samartho4@users.noreply.github.com>
2024-12-23 13:21:43 -0500

    Add solutions for Day 5 and Day 6

---
+++ Week1/Day 6/PromisesTime.js
@@ -0,0 +1,10 @@
+var addTwoPromises = async function(promise1, promise2) {
+    const value1 = await promise1;
+    const value2 = await promise2;
+
+    return value1 + value2;
+};
+
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
