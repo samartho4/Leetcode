78fd1a41e7f478c18b1232dfac4db5aad2e37c6f
samartho4 <142962370+samartho4@users.noreply.github.com>
2025-01-12 02:09:07 -0500

    Updated Week 3 Day 1 pandas6.py

---
+++ Week 3/Day 1/pandas6.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    result = customers.drop_duplicates(subset=["email"], keep="first")
+    return result
+
+    
